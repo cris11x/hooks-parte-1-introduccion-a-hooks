92abd379815dbd5cb254a795fa6ae209a6da8638
Cristian <cris10x@hotmail.com>
2021-03-22 14:06:48 -0500

    Optimizando codigo separando componentes

--- src/components/08-useRedeucer/TodoApp.js
+++ src/components/08-useRedeucer/TodoApp.js
@@ -1,140 +1,102 @@
-import React, { useEffect, useReducer } from 'react';
-import { todoReducer } from './todoReducer';
-import { useForm } from '../../hooks/useForm';
+import React, { useEffect, useReducer } from "react";
+import { todoReducer } from "./todoReducer";
+import { useForm } from "../../hooks/useForm";
 
-import './style.css';
+import "./style.css";
+import { TodoList } from "./TodoList";
 
 const init = () => {
-
-    return JSON.parse(localStorage.getItem('todos')) || [];
-
-    // console.log(localStorage.getItem('todos'));
-    // return [{
-    //     id: new Date().getTime(),
-    //     desc:'Aprender React',
-    //     donde: false
-    // }];
-}
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
 
 export const TodoApp = () => {
+  const [todos, dispatch] = useReducer(todoReducer, [], init);
 
-    const [ todos, dispatch ] = useReducer(todoReducer, [], init);
+  const [{ description }, handleInputChange, reset] = useForm({
+    description: "",
+  });
 
-    const [ { description }, handleInputChange, reset ] = useForm({
-        description: ''
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleDelete = (todoId) => {
+    const action = {
+      type: "delete",
+      payload: todoId,
+    };
+
+    dispatch(action);
+  };
+
+  const handleToggle = (todoId) => {
+    dispatch({
+      type: "toggle",
+      payload: todoId,
     });
+  };
 
-    useEffect(() => {
-        localStorage.setItem( 'todos', JSON.stringify( todos ) );
-    }, [todos])
+  const handleSubmit = (e) => {
+    e.preventDefault(); // evitar recargar la pagina al procesar el formulario
 
-    const handleDelete = ( todoId ) => {
-        
-        const action = {
-            type: 'delete',
-            payload: todoId
-        }
-
-        dispatch( action );
+    if (description.trim().length <= 1) {
+      return;
     }
 
-    const handleToggle = ( todoId ) => {
+    const newTodo = {
+      id: new Date().getTime(),
+      desc: description,
+      done: false,
+    };
 
-        dispatch({
-            type: 'toggle',
-            payload: todoId
-        })
+    const action = {
+      type: "add",
+      payload: newTodo,
+    };
 
+    dispatch(action);
+    reset();
+  };
 
-    }
+  return (
+    <div>
+      <h1>TodoApp ( {todos.length} )</h1>
+      <hr />
+      <div className="row">
+        <div className="col-7">
 
-    const handleSubmit = (e) => {
-        e.preventDefault(); // evitar recargar la pagina al procesar el formulario
-
-        if ( description.trim().length <= 1) {
-            return;
-        }
-
-        const newTodo = {
-            id: new Date().getTime(),
-            desc: description,
-            done: false
-        }
-
-        const action = {
-            type: 'add',
-            payload: newTodo
-        }
-
-        dispatch( action );
-        reset();
-
-    }
-
-
-    return (
-        <div>
-            <h1>TodoApp ( { todos.length } )</h1>
-            <hr />
-            <div className="row">
-
-                <div className="col-7">
-
-                    <ul className="list-group list-group-flush">
-                        {
-                            todos.map( (todo, i) => (
-                                <li
-                                    key={ todo.id }
-                                    className="list-group-item"
-                                >
-                                    <p 
-                                        className={ `${ todo.done && 'complete' }` }
-                                        onClick={ () => handleToggle( todo.id ) }
-                                    >
-                                        { i+1 }. { todo.desc }
-                                    </p>
-                                    <button
-                                        className="btn btn-danger"
-                                        onClick={ () => handleDelete( todo.id ) }
-                                    >
-                                        Borrar
-                                    </button>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                    
-                </div>
-
-                <div className="col-5">
-                    <h4>Agregar todo</h4>
-                    <hr />
-
-                    <form onSubmit={ handleSubmit }>
-                        <input 
-                            type="text"
-                            name="description"
-                            className="form-control"
-                            placeholder="Aprender ..."
-                            autoComplete="off"
-                            value = { description }
-                            onChange = { handleInputChange }
-                        />
-
-                        <button
-                            type="submit"
-                            className="btn btn-outline-primary mt-1 btn-block"
-                        >
-                            Agregar
-                        </button>
-                    </form>
-
-                </div>
-
-            </div>
-
-            
-            
+          {/* TodoList, todos, handleDelete, handleToogle */}
+          <TodoList
+            todos = { todos }
+            handleDelete = { handleDelete }
+            handleToggle = { handleToggle }
+          />
         </div>
-    )
-}
+
+        <div className="col-5">
+          <h4>Agregar todo</h4>
+          <hr />
+
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="description"
+              className="form-control"
+              placeholder="Aprender ..."
+              autoComplete="off"
+              value={description}
+              onChange={handleInputChange}
+            />
+
+            <button
+              type="submit"
+              className="btn btn-outline-primary mt-1 btn-block"
+            >
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
