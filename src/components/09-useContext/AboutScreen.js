d654752a20f768cebba574660d69105cbdcd8ef2
Cristian <cris10x@hotmail.com>
2021-03-24 11:49:44 -0500

    docs

--- src/components/09-useContext/AboutScreen.js
+++ src/components/09-useContext/AboutScreen.js
@@ -12,9 +12,7 @@ export const AboutScreen = () => {
     <div>
       <h1>AboutScreen</h1>
       <hr />
-
       <pre>{JSON.stringify(user, null, 3)};</pre>
-
       <button className="btn btn-warning" onClick={ handleClick }>
         Logout
       </button>
